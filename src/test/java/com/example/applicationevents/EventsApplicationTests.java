92cf7b0564c5ab009054de3598674b3c00ded402
prasaddharani <dharaniprasad1999@gmail.com>
2025-11-03 09:07:16 +0530

    Spring boot events inital commit

---
+++ src/test/java/com/example/applicationevents/EventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applicationevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
